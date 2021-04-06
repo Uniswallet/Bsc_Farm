19571580e077d677054ca86ea0577ac0412694b0
Uniswallet <72632599+Uniswallet@users.noreply.github.com>
2021-04-06 23:52:09 +0700

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,9 +10,9 @@
           </div>
           <div class="balance">
             <div class="balance__inner">
-              <Card title="bROOBEE/BNB" farm-link="/farming_pancake" top-icon="/pics/pancake.png" currency-icon="/pics/bnb.png" daily-pool="25 000" :finished="true" />
-              <Card title="bROOBEE/BNB" farm-link="/farming_bnb" top-icon="/pics/burger.png" currency-icon="/pics/bnb.png" daily-pool="25 000" />
-              <Card title="bROOBEE/BURGER" farm-link="/farming_burger" top-icon="/pics/burger.png" currency-icon="/pics/burger.png" daily-pool="25 000" />
+              <Card title="MBASE/BNB" farm-link="/farming_pancake" top-icon="/pics/pancake.png" currency-icon="/pics/bnb.png" daily-pool="25 000" :finished="true" />
+              <Card title="MBASE/BNB" farm-link="/farming_bnb" top-icon="/pics/burger.png" currency-icon="/pics/bnb.png" daily-pool="25 000" />
+              <Card title="MBASE/BURGER" farm-link="/farming_burger" top-icon="/pics/burger.png" currency-icon="/pics/burger.png" daily-pool="25 000" />
             </div>
             <div class="balance__info">
               <div class="balance__icon">!</div>
